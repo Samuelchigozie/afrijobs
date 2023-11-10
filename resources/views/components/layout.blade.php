895701b7c0d00e0250e3226dab85dbf5424d1cc0
samuel chigozie <nzekwesammy@gmail.com>
2023-11-10 09:00:23 -0800

    updated footer and logo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -76,7 +76,9 @@
     </main>
 
     <footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-red-500 text-white h-24 mt-24 opacity-90 md:justify-center">
-        <p class="ml-2">Copyright &copy; 2023, Developed by Samuel</p>
+        <p class="ml-2 hidden md:block">Copyright &copy; 2023, Developed by Samuel</p>
+        <p class="ml-2 block md:hidden">Copyright &copy; 2023, Developed by <br>Samuel</p>
+
 
         <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">
             Post Job
